0f73571f21bdda8a735a9ab85cc84a825293cd4c
Leonid Radkevich <laradkevich@gmail.com>
2026-02-11 22:28:31 +0400

    Breakin the app

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,33 +1,4 @@
 ﻿using ConsoleApp8;
-
-Console.WriteLine("=== Battle Simulation System ===\n");
-
-var battle = new Battle();
-
-battle.LoadNamesFromFile("names.txt");
-
-Console.Write("Enter number of squads per faction: ");
-int squadCount = int.Parse(Console.ReadLine() ?? "2");
-
-Console.Write("Enter number of fighters per squad: ");
-int fightersPerSquad = int.Parse(Console.ReadLine() ?? "10");
-
-battle.InitializeFactions(squadCount, fightersPerSquad);
-
-battle.ShowFactionsReport();
-
-while (true)
-{
-    Console.WriteLine("\n=== BATTLE MENU ===");
-    Console.WriteLine("1. PvP Battle (1 vs 1)");
-    Console.WriteLine("2. Squad vs Squad");
-    Console.WriteLine("3. Faction War (Coming soon)");
-    Console.WriteLine("0. Exit");
-    Console.Write("\nSelect option: ");
-
-    string? input = Console.ReadLine();
-    
-    switch (input)
     {
         case "1":
             battle.PvPBattle();
